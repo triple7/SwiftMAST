6932b471f56744dc86a7140692836e28fceb5379
Yuma Antoine Decaux <yuma@astreos.space>
2022-12-29 15:08:25 +1000

    Added FitsCore working

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
             targets: ["SwiftMAST"]),
     ],
     dependencies: [
-//         .package(url: "https://github.com/brampf/fitscore", from: "0.1.0"),
-    ],
+        .package(url: "https://github.com/brampf/fitscore", .upToNextMajor(from: "0.3.0")),    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
