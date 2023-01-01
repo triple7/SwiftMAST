c82041b15c426cff1dc509a5b5e26cf2fd7c7031
Yuma Antoine Decaux <yuma@astreos.space>
2023-01-01 21:56:44 +1000

    Added FitsKit + minimum mac os version 11 IOS 14

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftMAST",
+    platforms: [
+        .iOS("14"),
+        .macOS("11")
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,13 +16,13 @@ let package = Package(
             targets: ["SwiftMAST"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/brampf/fitscore.git", branch: "master"),.package(url: "https://github.com/apple/swift-numerics.git", branch: "main")],
+        .package(url: "https://github.com/brampf/fitscore.git", branch: "master"), .package(url: "https://github.com/brampf/fitskit.git", branch: "master"), .package(url: "https://github.com/apple/swift-numerics.git", branch: "main")],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftMAST",
-            dependencies: [.product(name: "FITSCore", package: "FITSCore"), .product(name: "Numerics", package: "swift-numerics")]),
+            dependencies: [.product(name: "FITSCore", package: "FITSCore"), .product(name: "FITSKit", package: "FITSKit"), .product(name: "Numerics", package: "swift-numerics")]),
         .testTarget(
             name: "SwiftMASTTests",
             dependencies: ["SwiftMAST", .product(name: "FITSCore", package: "FITSCore")]),
