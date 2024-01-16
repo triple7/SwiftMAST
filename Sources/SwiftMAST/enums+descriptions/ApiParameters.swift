b56e7473070485f8254e1360b568576fee2e5c6c
Yuma Antoine Decaux <yuma@astreos.space>
2024-01-17 08:32:19 +1000

    tineout changed from 3 t o20

--- Sources/SwiftMAST/enums+descriptions/ApiParameters.swift
+++ Sources/SwiftMAST/enums+descriptions/ApiParameters.swift
@@ -52,7 +52,7 @@ case page // Int
      Convenience function for optimising search results
      */
     public func defaultGeneralParameters()->[MAP: Any] {
-        return [MAP.pagesize: 100, MAP.timeout: 3, MAP.removenullcolumns: true]
+        return [MAP.pagesize: 100, MAP.timeout: 20, MAP.removenullcolumns: true]
     }
 }
 
