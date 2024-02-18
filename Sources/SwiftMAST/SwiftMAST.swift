20fdb5119c79d2ebbcfdf54122ee9c699dc52a78
Yuma Antoine Decaux <yuma@astreos.space>
2024-02-19 09:12:11 +1000

    Changed print object for percentage downloaded

--- Sources/SwiftMAST/SwiftMAST.swift
+++ Sources/SwiftMAST/SwiftMAST.swift
@@ -136,7 +136,7 @@ public class SwiftMAST:NSObject {
          buffer += data.count
          let percentageDownloaded = Float(buffer) / Float(expectedContentLength!)
             progress =  percentageDownloaded
-         print(progress)
+         print(percentageDownloaded)
      }
 
 }
