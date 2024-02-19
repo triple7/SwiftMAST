a6012f81c256c2d1e0ea89ccff0517deebd5e11f
Yuma Antoine Decaux <yuma@astreos.space>
2024-02-19 10:00:28 +1000

    some urls are returned empty from search in mast

--- Sources/SwiftMAST/SwiftMAST+request.swift
+++ Sources/SwiftMAST/SwiftMAST+request.swift
@@ -79,7 +79,7 @@ closure(false)
      */
     func requestProductBundle(service: Service, coamResults: [CoamResult],_ closure: @escaping (Bool, [URL]) -> Void) {
         print("requestProductBundle: getting \(coamResults.count) URLs in tar.gz bundle")
-        let urls = coamResults.map{["uri", $0.jpegURL]}
+        let urls = coamResults.map{["uri", $0.dataURL]}
         for u in urls {
             print("url: \(u)")
         }
