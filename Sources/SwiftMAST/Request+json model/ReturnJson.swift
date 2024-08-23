d0bd4abb73e5883a802a52e3dc0e86db812190b8
Yuma Antoine Decaux <yuma@astreos.space>
2024-08-23 15:23:37 +1000

    enum QValue description remove

--- Sources/SwiftMAST/Request+json model/ReturnJson.swift
+++ Sources/SwiftMAST/Request+json model/ReturnJson.swift
@@ -29,7 +29,7 @@ public enum QValue: Hashable, Identifiable {
         }
     }
 }
-extension QValue:Codable, CustomStringConvertible {
+extension QValue:Codable {
 
     public init(from decoder: Decoder) throws {
         if let intValue = try? decoder.singleValueContainer().decode(Int.self) {
@@ -100,9 +100,9 @@ extension QValue:Codable, CustomStringConvertible {
         }
     }
     
-    public var description: String {
-        return "\(self.value)"
-    }
+//    public var description: String {
+//        return "\(self.value)"
+//    }
     
 }
 
