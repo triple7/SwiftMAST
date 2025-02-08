9a30798df1a2f58d0dbe6e4498529d859a272d56
Yuma Antoine Decaux <yuma@astreos.space>
2025-02-09 06:39:45 +1000

    Adding double quotes

--- Sources/SwiftMAST/SwiftMAST+request.swift
+++ Sources/SwiftMAST/SwiftMAST+request.swift
@@ -383,7 +383,10 @@ func queryMast(service: Service, params: MASTJson, returnType: APIReturnType, _ 
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 
-        let bodyParameters = "QUERY=\(mASTTapRequest.getSelectQuery().addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")&LANG=ADQL-2.0&format=json"
+        let query = mASTTapRequest.getSelectQuery()
+        let bodyParameters = "QUERY=\(query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")&LANG=ADQL-2.0&format=json"
+        request.httpBody = bodyParameters.data(using: .utf8)
+
         request.httpBody = bodyParameters.data(using: .utf8)
 
         if token != nil {
